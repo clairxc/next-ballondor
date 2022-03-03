02f3444f53f6c1e9731d5eaa1225020aefa2a22a
clairxc <clairrrc@gmail.com>
2022-03-02 16:21:21 -0800

    display teams based on querysearch

--- controllers/teams.js
+++ controllers/teams.js
@@ -13,27 +13,71 @@ require('dotenv').config() // this is so that our .process.env.SECRET works
 // })
 
 // premier league
-router.get("/", (req, res) => {
+// router.get("/premier", (req, res) => {
+//   // console.log(req.query.q)
+//   const url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League`;
+//   // console.log(url)
+//   const leagueTeams = [];
+//   axios.get(url).then((response) => {
+//     // console.log(response.data.Search)
+//     const teams = [...response.data.teams];
+//     // console.log(teams)
+//     teams.forEach((team) => {
+//       leagueTeams.push({
+//         name: team.strTeam
+//       });
+//     })
+//     console.log(leagueTeams)
+//     // res.send(leagueTeams)
+//     res.render("teams/teams.ejs", {
+//       leagueTeams: leagueTeams
+//     });
+//   });
+// });
+
+// router.get("/bundesliga", (req, res) => {
+//   // console.log(req.query.q)
+//   const url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=German%20Bundesliga`;
+//   // console.log(url)
+//   const leagueTeams = [];
+//   axios.get(url).then((response) => {
+//     // console.log(response.data.Search)
+//     const teams = [...response.data.teams];
+//     // console.log(teams)
+//     teams.forEach((team) => {
+//       leagueTeams.push({
+//         name: team.strTeam
+//       });
+//     })
+//     console.log(leagueTeams)
+//     // res.send(leagueTeams)
+//     res.render("teams/teams.ejs", {
+//       leagueTeams: leagueTeams
+//     });
+//   });
+// });
+
+router.get("/:name", (req, res) => {
   // console.log(req.query.q)
-  const url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League`;
-  // console.log(url)
+  const url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=${req.params.name}`;
+  console.log(url)
   const leagueTeams = [];
   axios.get(url).then((response) => {
-    // console.log(response.data.Search)
+    console.log(response)
     const teams = [...response.data.teams];
-    // console.log(teams)
+    // // console.log(teams)
     teams.forEach((team) => {
       leagueTeams.push({
         name: team.strTeam
       });
     })
-    console.log(leagueTeams)
-    res.send(leagueTeams)
+    // console.log(leagueTeams)
+    // // res.send(leagueTeams)
+    res.render("teams/teams.ejs", {
+      leagueTeams: leagueTeams
     });
-    // res.render("teams/teams.ejs", {
-    //   leagueTeams: leagueTeams
-    // });
   });
+})
 
 // router.get("/", (req, res) => {
 //   const url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php`;
